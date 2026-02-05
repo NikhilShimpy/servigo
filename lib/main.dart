972ba2976df9672d3faa58e9c73657d78ff574d7
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-02-05 22:26:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'authentication/login.dart';
-import 'authentication/register.dart';
+
 import 'home.dart';
 
 void main() async {
