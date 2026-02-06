ca8087760cada360e1cd1aa6144689e4ace90c4d
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-02-06 08:03:56 +0530

    Update login.dart

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 
 class MyLogin extends StatefulWidget {
